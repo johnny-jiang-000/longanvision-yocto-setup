e4d6dd43300b4420034f9160322436d2d1fae938
johnny-jiang-000 <johnnyjiang@longanvision.com>
2020-06-30 21:14:29 +0000

    first commit

---
+++ coral-dev/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+if [ "$1" != "" ]; then
+    echo "coral-dev yocto root directory is $1"
+    cp conf/*.conf $1/build/conf
+    cp *.gir $1/build/tmp/work/aarch64-fslc-linux/clutter-gst-3.0/3.0.27-r0/recipe-sysroot/usr/share/gir-1.0
+    echo 'CONFIG_LOCALVERSION="-coral"' >> $1/sources/meta-coral/meta-coral-bsp/recipes-kernel/linux/linux-imx/coral-dev/defconfig
+    echo 'CONFIG_EXTRA_FIRMWARE_DIR="firmware"' >> $1/sources/meta-coral/meta-coral-bsp/recipes-kernel/linux/linux-imx/coral-dev/defconfig
+    echo 'CONFIG_EXTRA_FIRMWARE="sdma-imx7d.bin"' >> $1/sources/meta-coral/meta-coral-bsp/recipes-kernel/linux/linux-imx/coral-dev/defconfig
+    cp *.bin $1/build/tmp/work-shared/coral-dev/kernel-source/firmware
+
+
+else
+    echo "root directory of coral-dev yocto is required"
+fi
